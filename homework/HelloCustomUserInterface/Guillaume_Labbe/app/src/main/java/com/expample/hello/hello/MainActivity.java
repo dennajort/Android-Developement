90031cb0c41096e8198e473a4847f4ba01614095
roberto69 <guillaume.lab@live.fr>
2014-12-19 17:11:01 +0800

    ui guillaume

---
+++ homework/HelloCustomUserInterface/Guillaume_Labbe/app/src/main/java/com/expample/hello/hello/MainActivity.java
@@ -0,0 +1,25 @@
+package com.expample.hello.hello;
+
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.internal.widget.AdapterViewCompat;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+}
